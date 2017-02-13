835ac17953b3030fae650849a2e1d788abd5ab68
outman1992 <outman584845663@gmail.com>
2017-02-13 17:53:46 +0800

    获取照片，网络请求

--- src/pages/publish/publish.ts
+++ src/pages/publish/publish.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 import { LoginPage } from '../usercenter/login';
-import { NavController, ModalController, ViewController } from 'ionic-angular';
-import { Camera, ImagePicker } from 'ionic-native';
+import { NavController, ModalController, ViewController, ActionSheetController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 @Component({
     selector: 'page-publish',
@@ -10,12 +10,20 @@ import { Camera, ImagePicker } from 'ionic-native';
 })
 export class PublishPage {
 
+    options: any = {
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA
+    };
+
+    imageArray: any;
+
     constructor(
         public navCtrl: NavController,
         public modalCtrl: ModalController,
-        public viewCtrl: ViewController
+        public viewCtrl: ViewController,
+        public actionSheetCtrl: ActionSheetController,
     ) {
-
+        this.imageArray = [];
     }
 
     // checkLogin(){
@@ -33,28 +41,50 @@ export class PublishPage {
     }
 
     getImages() {
-        alert(JSON.stringify(Camera));
-
-        Camera.getPicture().then((imageData) => {
-            // imageData is either a base64 encoded string or a file URI
-            // If it's base64:
-            let base64Image = 'data:image/jpeg;base64,' + imageData;
-            alert(base64Image);
-        }, (err) => {
-            alert(err);
-            // Handle error
+        let actionSheet = this.actionSheetCtrl.create({
+            title: '选择图片',
+            buttons: [
+                {
+                    text: '从相册中选取',
+                    // role: 'destructive',
+                    handler: () => {
+                        // console.log('Destructive clicked');
+                        this.options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+                        this.getPhotos();
+                    }
+                }, {
+                    text: '拍照',
+                    handler: () => {
+                        // console.log('Archive clicked');
+                        this.options.sourceType = Camera.PictureSourceType.CAMERA;
+                        this.getPhotos();
+                    }
+                }, {
+                    text: '取消',
+                    role: 'cancel',
+                    handler: () => {
+                        // console.log('Cancel clicked');
+                    }
+                }
+            ]
         });
+        actionSheet.present();
 
     }
 
     getPhotos(){
-        alert(JSON.stringify(ImagePicker));
-        ImagePicker.getPictures().then((results) => {
-            for (var i = 0; i < results.length; i++) {
-                console.log('Image URI: ' + results[i]);
-            }
-        }, (err) => { 
+        // this.imageArray.push('http://photocdn.sohu.com/20091011/Img267278341.jpg');
+        // debugger
+        Camera.getPicture(this.options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+            // let base64Image = imageData;
+            this.imageArray.push(base64Image);
+            alert(base64Image);
+        }, (err) => {
             alert(err);
+            // Handle error
         });
     }
 
--- src/pages/usercenter/login.ts
+++ src/pages/usercenter/login.ts
@@ -1,55 +1,84 @@
 import { Component } from '@angular/core';
 
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { NavController, ViewController, ToastController, LoadingController } from 'ionic-angular';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
-  	private user: any;
-	constructor(
-		public navCtrl: NavController, 
-		public viewCtrl: ViewController, 
-		private toastCtrl: ToastController,
-		public loadingCtrl: LoadingController
-	) {
-		this.user = {};
-		this.user.username = "";
-		this.user.password = "";
-	}
+    private user: any;
+    constructor(
+        public navCtrl: NavController,
+        public viewCtrl: ViewController,
+        private toastCtrl: ToastController,
+        public loadingCtrl: LoadingController,
+        public http: Http,
+    ) {
+        this.user = {};
+        this.user.username = "";
+        this.user.password = "";
+    }
 
-	login(){
-		var toastOpt = {
-			message: "",
-			duration: 3000,
-  			position: 'middle'
-		}
-		if(this.user.username == "" || this.user.username.length <= 3){
-			toastOpt.message = "请输入正确的用户名";
-			let toast = this.toastCtrl.create(toastOpt);
-			toast.present();
-		}else if(this.user.password == "" || this.user.password.length <= 6){
-			toastOpt.message = "请输入正确的密码";
-			let toast = this.toastCtrl.create(toastOpt);
-			toast.present();
-		}else{
-			console.log(this.user);
-			let loading = this.loadingCtrl.create({
-				spinner: 'circles'
-			});
+    login() {
+        var toastOpt = {
+            message: "",
+            duration: 3000,
+            position: 'middle'
+        }
+        if (this.user.username == "" || this.user.username.length <= 3) {
+            toastOpt.message = "请输入正确的用户名";
+            let toast = this.toastCtrl.create(toastOpt);
+            toast.present();
+        } else if (this.user.password == "" || this.user.password.length <= 6) {
+            toastOpt.message = "请输入正确的密码";
+            let toast = this.toastCtrl.create(toastOpt);
+            toast.present();
+        } else {
+            // console.log(this.user);
+            let loading = this.loadingCtrl.create({
+                spinner: 'circles'
+            });
 
-			loading.present();
-			localStorage.setItem('token', '123456789789456123');
+            loading.present();
+            let userInfo = {
+                username: this.user.username,
+                password: Md5.hashStr(this.user.password)
+            }
 
-			setTimeout(() => {
-				loading.dismiss();
-				this.viewCtrl.dismiss();
-			}, 1000);
-		}
-	}
+            debugger
+            let headers = new Headers({ 'Content-Type': 'application/json' }); //其实不表明 json 也可以, ng 默认好像是 json
+            let options = new RequestOptions({ headers: headers });
+            this.http.post('http://www.wanlinqiang.com/login', JSON.stringify(userInfo), options).subscribe((data) => {
+                let Data = data.json();
 
-	goback(){
-		this.viewCtrl.dismiss();
-	}
+                if (Data.success) {
+                    toastOpt.message = Data.message;
+                    let toast = this.toastCtrl.create(toastOpt);
+                    toast.present();
+
+                    localStorage.setItem('token', '123456789789456123');
+
+                    setTimeout(() => {
+                        loading.dismiss();
+                        this.viewCtrl.dismiss();
+                    }, 1000);
+                } else {
+                    toastOpt.message = Data.message;
+                    let toast = this.toastCtrl.create(toastOpt);
+                    toast.present();
+                }
+
+
+            })
+
+
+        }
+    }
+
+    goback() {
+        this.viewCtrl.dismiss();
+    }
 }
