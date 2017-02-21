5167356fb96b4fed374dc47524cbeb0d7b5ed8d5
outman1992 <outman584845663@gmail.com>
2017-02-21 18:07:11 +0800

    app首页焦点图请求，全局请求地址的配置

---
+++ src/app/app.config.ts
@@ -0,0 +1,7 @@
+export class AppConfig {
+
+	//生产环境URL
+	public static getProdUrl() {
+		return "http://127.0.0.1:3000";
+	}
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,119 +4,134 @@ import { Http } from '@angular/http';
 import { App, ViewController, NavController, ModalController } from 'ionic-angular';
 import { DetailPage } from './detail';
 import { SearchPage } from '../search/search';
+import { AppConfig } from '../../app/app.config';
 
 @Component({
-    selector: 'page-home',
-    templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-    focus: any;
-    category: any;
-    current: string;
-    goods: any;
-    page: any;
-    dataOver: any;
+	focus: any;
+	category: any;
+	current: string;
+	goods: any;
+	page: any;
+	dataOver: any;
+	api: String = AppConfig.getProdUrl();
 
-    constructor(
-        public navCtrl: NavController,
-        public appCtrl: App,
-        public viewCtrl: ViewController,
-        public http: Http,
-        public modalCtrl: ModalController,
-    ) {
-        this.focus = [
-            { img: 'http://www.taoertao.com/attachment/focus/1459485773jxjp.jpg' },
-            { img: 'http://www.taoertao.com/attachment/focus/banner-ghm.png' },
-            { img: 'http://www.taoertao.com/attachment/focus/1426832637hqebp.jpg' }
-        ];
-        this.category = {
-            cateLineOne: [
-                { icon: 'phone-portrait', cateName: '手机' },
-                { icon: 'laptop', cateName: '电脑' },
-                { icon: 'headset', cateName: '配件' },
-                { icon: 'bulb', cateName: '电器' },
-            ],
-            cateLineTwo: [
-                { icon: 'book', cateName: '书籍' },
-                { icon: 'game-controller-a', cateName: '娱乐' },
-                { icon: 'football', cateName: '运动' },
-                { icon: 'bicycle', cateName: '代步' },
-            ]
-        };
-        this.current = 'new';    //new: 最新，recommend: 推荐
+	constructor(
+		public navCtrl: NavController,
+		public appCtrl: App,
+		public viewCtrl: ViewController,
+		public http: Http,
+		public modalCtrl: ModalController,
+	) {
+		this.focus = [
+			{ img: 'http://www.taoertao.com/attachment/focus/1459485773jxjp.jpg' },
+			{ img: 'http://www.taoertao.com/attachment/focus/banner-ghm.png' },
+			{ img: 'http://www.taoertao.com/attachment/focus/1426832637hqebp.jpg' }
+		];
+		this.getFocus()
 
-        //加载初始数据
-        this.loadGoodList();
+		this.category = {
+			cateLineOne: [
+				{ icon: 'phone-portrait', cateName: '手机' },
+				{ icon: 'laptop', cateName: '电脑' },
+				{ icon: 'headset', cateName: '配件' },
+				{ icon: 'bulb', cateName: '电器' },
+			],
+			cateLineTwo: [
+				{ icon: 'book', cateName: '书籍' },
+				{ icon: 'game-controller-a', cateName: '娱乐' },
+				{ icon: 'football', cateName: '运动' },
+				{ icon: 'bicycle', cateName: '代步' },
+			]
+		};
+		this.current = 'new';    //new: 最新，recommend: 推荐
 
-        this.page = 1;
-        this.dataOver = false;
-    }
+		//加载初始数据
+		this.loadGoodList();
 
-    pushCateList(cateName) {
-        // console.log(cateName)
-    }
+		this.page = 1;
+		this.dataOver = false;
+	}
 
-    pushDetail(id) {
-        // this.viewCtrl.dismiss();
-        this.appCtrl.getRootNav().push(DetailPage, { id: id });
-        // this.navCtrl.push(DetailPage, { id: id });
-    }
+	getFocus() {
+		// debugger
+		this.http.get(this.api + '/app/get_focus').subscribe((data) => {
+			if (data.json().success) {
+				this.focus = data.json().result
+				return true
+			}
+			return false
+		})
+	}
 
-    doInfinite(infiniteScroll) {
+	pushCateList(cateName) {
+		// console.log(cateName)
+	}
 
-        // debugger
-        if (this.page <= 2) {
-            this.page++
-            
-            setTimeout(() => {
-                this.http.get('http://rap.taobao.org/mockjsdata/8438/api/index/goods').subscribe(data => {
-                    // console.log(data.json().result.data);
-                    // this.goods.concat(data.json().result.data);
-                    // console.log(this.goods);
-                    var that = this;
-                    data.json().result.data.forEach(function(v){
-                        that.goods.push(v)
-                    })
-                    // console.log(this.goods);
-                    // console.log(typeof that.goods)
-                    // console.log(data.json().result)
-                    // console.log('Async operation has ended');
-                    infiniteScroll.complete();
-                });
+	pushDetail(id) {
+		// this.viewCtrl.dismiss();
+		this.appCtrl.getRootNav().push(DetailPage, { id: id });
+		// this.navCtrl.push(DetailPage, { id: id });
+	}
 
-                // console.log('Async operation has ended');
-                // infiniteScroll.complete();
-            }, 500);
-        }else{
-            this.dataOver = true;
-            infiniteScroll.enable(false);
-        }
-    }
+	doInfinite(infiniteScroll) {
 
-    loadList(type){
-        if(type == 1){
-            this.current = 'new';
-            this.loadGoodList();
-        } else if (type == 2) {
-            this.current = 'recommend';
-            this.loadGoodList();
-        }
-    }
+		// debugger
+		if (this.page <= 2) {
+			this.page++
 
-    //加载数据方法
-    loadGoodList(){
-        this.http.get('http://rap.taobao.org/mockjsdata/8438/api/index/goods').subscribe(data => {
-            this.goods = data.json().result.data;
-            // console.log(typeof that.goods)
-            // console.log(data.json().result)
-            this.dataOver = false;
-            this.page = 1;
-        });
-    }
+			setTimeout(() => {
+				this.http.get('http://rap.taobao.org/mockjsdata/8438/api/index/goods').subscribe(data => {
+					// console.log(data.json().result.data);
+					// this.goods.concat(data.json().result.data);
+					// console.log(this.goods);
+					var that = this;
+					data.json().result.data.forEach(function (v) {
+						that.goods.push(v)
+					})
+					// console.log(this.goods);
+					// console.log(typeof that.goods)
+					// console.log(data.json().result)
+					// console.log('Async operation has ended');
+					infiniteScroll.complete();
+				});
 
-    openSearchModal(){
-        let searchModal = this.modalCtrl.create(SearchPage);
-        searchModal.present();
-    }
+				// console.log('Async operation has ended');
+				// infiniteScroll.complete();
+			}, 500);
+		} else {
+			this.dataOver = true;
+			infiniteScroll.enable(false);
+		}
+	}
+
+	loadList(type) {
+		if (type == 1) {
+			this.current = 'new';
+			this.loadGoodList();
+		} else if (type == 2) {
+			this.current = 'recommend';
+			this.loadGoodList();
+		}
+	}
+
+	//加载数据方法
+	loadGoodList() {
+		this.http.get('http://rap.taobao.org/mockjsdata/8438/api/index/goods').subscribe(data => {
+			this.goods = data.json().result.data;
+			// console.log(typeof that.goods)
+			// console.log(data.json().result)
+			this.dataOver = false;
+			this.page = 1;
+		});
+	}
+
+	openSearchModal() {
+		let searchModal = this.modalCtrl.create(SearchPage);
+		searchModal.present();
+	}
 }
